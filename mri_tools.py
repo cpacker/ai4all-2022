bf225038e2a4bbc565d16d57c1456455d9967f9f
spetryk <spetryk@berkeley.edu>
2021-08-04 11:27:49 -0700

    init from updated 2020 files

---
+++ mri_tools.py
@@ -0,0 +1,216 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+import nilearn
+from nilearn import image
+
+
+import os
+
+"""
+import line_profiler
+import atexit
+profile = line_profiler.LineProfiler()
+atexit.register(profile.print_stats)
+"""
+
+class BrainomicsDataset(Dataset):
+    def __init__(self, dataset_dir):
+        self.dataset_dir = dataset_dir
+
+        self.data = torch.load(os.path.join(dataset_dir, "mris.pt"))
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def __len__(self):
+        return self.data.size(0)
+        
+def get_brainomics_dataloader(base_dir, batch_size=64):
+    """
+    all_transforms = transforms.Compose([
+        transforms.Normalize(0, 32768),
+        transforms.ToPILImage(),
+        transforms.CenterCrop([240, 128]),
+        transforms.ToTensor(),
+        #transforms.Normalize(2635, 3561),
+    ])
+    """
+
+    #base_dir = "/work/drothchild/datasets/brainomics/localizer"
+    data = BrainomicsDataset(base_dir)
+    loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    return loader
+
+def get_mnist_dataloaders(batch_size=128):
+    """MNIST dataloader with (32, 32) sized images."""
+    # Resize images so they are a power of 2
+    all_transforms = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor()
+    ])
+    # Get train and test data
+    train_data = datasets.MNIST('../data', train=True, download=True,
+                                transform=all_transforms)
+    test_data = datasets.MNIST('../data', train=False,
+                               transform=all_transforms)
+    # Create dataloaders
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
+
+
+def get_fashion_mnist_dataloaders(batch_size=128):
+    """Fashion MNIST dataloader with (32, 32) sized images."""
+    # Resize images so they are a power of 2
+    all_transforms = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor()
+    ])
+    # Get train and test data
+    train_data = datasets.FashionMNIST('../fashion_data', train=True, download=True,
+                                       transform=all_transforms)
+    test_data = datasets.FashionMNIST('../fashion_data', train=False,
+                                      transform=all_transforms)
+    # Create dataloaders
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
+
+
+def get_lsun_dataloader(path_to_data='../lsun', dataset='bedroom_train',
+                        batch_size=64):
+    """LSUN dataloader with (128, 128) sized images.
+
+    path_to_data : str
+        One of 'bedroom_val' or 'bedroom_train'
+    """
+    # Compose transforms
+    transform = transforms.Compose([
+        transforms.Resize(128),
+        transforms.CenterCrop(128),
+        transforms.ToTensor()
+    ])
+
+    # Get dataset
+    lsun_dset = datasets.LSUN(db_path=path_to_data, classes=[dataset],
+                              transform=transform)
+
+    # Create dataloader
+    return DataLoader(lsun_dset, batch_size=batch_size, shuffle=True)
+
+class Generator(nn.Module):
+    def __init__(self, img_size, latent_dim, dim=16):
+        super(Generator, self).__init__()
+
+        self.dim = dim
+        self.latent_dim = latent_dim
+        self.img_size = img_size
+        self.feature_sizes = (self.img_size[0] // 16, self.img_size[1] // 16)
+
+        self.latent_to_features = nn.Sequential(
+            nn.Linear(latent_dim, 8 * dim * self.feature_sizes[0] * self.feature_sizes[1]),
+            nn.ReLU()
+        )
+
+        self.features_to_image = nn.Sequential(
+            nn.ConvTranspose2d(8 * dim, 4 * dim, 4, 2, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(4 * dim),
+            nn.ConvTranspose2d(4 * dim, 2 * dim, 4, 2, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(2 * dim),
+            nn.ConvTranspose2d(2 * dim, dim, 4, 2, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(dim),
+            nn.ConvTranspose2d(dim, self.img_size[2], 4, 2, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_data):
+        # Map latent into appropriate size for transposed convolutions
+        x = self.latent_to_features(input_data)
+        # Reshape
+        x = x.view(-1, 8 * self.dim, self.feature_sizes[0], self.feature_sizes[1])
+        # Return generated image
+        return self.features_to_image(x)
+
+    def sample_latent(self, num_samples):
+        return torch.randn((num_samples, self.latent_dim))
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_size, dim=16):
+        """
+        img_size : (int, int, int)
+            Height and width must be powers of 2.  E.g. (32, 32, 1) or
+            (64, 128, 3). Last number indicates number of channels, e.g. 1 for
+            grayscale or 3 for RGB
+        """
+        super(Discriminator, self).__init__()
+
+        self.img_size = img_size
+
+        self.image_to_features = nn.Sequential(
+            nn.Conv2d(self.img_size[2], dim, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(dim, 2 * dim, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(2 * dim, 4 * dim, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(4 * dim, 8 * dim, 4, 2, 1),
+            nn.Sigmoid()
+        )
+
+        # 4 convolutions of stride 2, i.e. halving of size everytime
+        # So output size will be 8 * (img_size / 2 ^ 4) * (img_size / 2 ^ 4)
+        output_size = 8 * dim * (img_size[0] // 16) * (img_size[1] // 16)
+        self.features_to_prob = nn.Sequential(
+            nn.Linear(output_size, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_data):
+        batch_size = input_data.size()[0]
+        x = self.image_to_features(input_data)
+        x = x.view(batch_size, -1)
+        return self.features_to_prob(x)
+
+
+def update(opt, loss):
+    loss.backward()
+    opt.step()
+
+def gradient_penalty(discriminator, real_images, generated_images):
+    batch_size = real_images.size()[0]
+
+    # Calculate interpolation
+    alpha = torch.rand(batch_size, 1, 1, 1)
+    alpha = alpha.expand_as(real_images).cuda()
+    interpolated = alpha * real_images + (1 - alpha) * generated_images
+    interpolated.requires_grad = True
+    interpolated = interpolated.cuda()
+
+    # Calculate probability of interpolated examples
+    prob_interpolated = discriminator(interpolated)
+
+    # Calculate gradients of probabilities with respect to examples
+    gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
+                           grad_outputs=torch.ones(prob_interpolated.size()).cuda(),
+                           create_graph=True, retain_graph=True)[0]
+
+    # Gradients have shape (batch_size, num_channels, img_width, img_height),
+    # so flatten to easily take norm per example in batch
+    gradients = gradients.view(batch_size, -1)
+    #self.losses['gradient_norm'].append(gradients.norm(2, dim=1).mean().data)
+
+    # Derivatives of the gradient close to 0 can cause problems because of
+    # the square root, so manually calculate norm and add epsilon
+    gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
+
+    # Return gradient penalty
+    return 10 * ((gradients_norm - 1) ** 2).mean()
+
